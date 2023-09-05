a953fcef03c9e1a995ed60206172e3753b7471bf
orayanics <nicole.oraya.cics@ust.edu.ph>
2023-09-05 13:01:24 +0800

    Add project files.

---
+++ OrayaAct/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrayaAct.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
