645a7887606e93e089759a228187010a3fea814e
orayanics <nicole.oraya.cics@ust.edu.ph>
2023-09-11 11:07:12 +0800

    added machine activity and lab activity

---
+++ OrayaAct/Controllers/InstructorController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrayaAct.Models;
+
+namespace OrayaAct.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+        {
+            new Instructor()
+            {
+                Id = 1,
+                FirstName = "Gabriel",
+                LastName = "Montano",
+                IsTenured = true,
+                Rank = Rank.Instructor,
+                HiringDate = DateTime.Parse("2022/05/21")
+            },
+            new Instructor()
+            {
+                Id = 2,
+                FirstName = "Mia",
+                LastName = "Eleazar",
+                IsTenured = true,
+                Rank = Rank.AssistantProfessor,
+                HiringDate = DateTime.Parse("2021/05/21")
+            },
+            new Instructor()
+            {
+                Id = 3,
+                FirstName = "Arthur",
+                LastName = "Ollanda",
+                IsTenured = true,
+                Rank = Rank.AssociateProfessor,
+                HiringDate = DateTime.Parse("2019/05/21")
+            },
+            new Instructor()
+            {
+                Id = 4,
+                FirstName = "Leonid",
+                LastName = "Lintag",
+                IsTenured = true,
+                Rank = Rank.Professor,
+                HiringDate = DateTime.Parse("2019/05/21")
+            },
+        };
+        public IActionResult Index()
+        {
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            Instructor? instructor = InstructorList.FirstOrDefault(x => x.Id == id);
+
+            if (instructor != null)
+            {
+                return View(instructor);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ OrayaAct/Controllers/StudentController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrayaAct.Models;
+
+namespace OrayaAct.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> StudentList = new List<Student>
+        {
+            new Student()
+            {
+                Id = 1,
+                FirstName = "Nicole",
+                LastName = "Oraya",
+                GPA = 1.4,
+                Course = Course.BSIT,
+                AdmissionDate = DateTime.Parse("2022/06/27"),
+                Email = "nicole.oraya.cics@ust.edu.ph"
+            },
+            new Student()
+            {
+                Id = 2,
+                FirstName = "Miguel",
+                LastName = "Oraya",
+                GPA = 1.5,
+                Course = Course.BSIS,
+                AdmissionDate = DateTime.Parse("2019/05/27"),
+                Email = "miguel.oraya.cics@ust.edu.ph"
+            },
+            new Student()
+            {
+                Id = 3,
+                FirstName = "Josephine",
+                LastName = "Oraya",
+                GPA = 1.3,
+                Course = Course.BSCS,
+                AdmissionDate = DateTime.Parse("2018/02/1"),
+                Email = "josephine.oraya.cics@ust.edu.ph"
+            },
+            new Student()
+            {
+                Id = 4,
+                FirstName = "Bada",
+                LastName = "Lee",
+                GPA = 1.4,
+                Course = Course.OTHER,
+                AdmissionDate = DateTime.Parse("2022/06/28"),
+                Email = "bada.lee.cics@ust.edu.ph"
+            }
+
+        };
+
+
+        public IActionResult Index()
+        {
+            return View(StudentList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            Student? student = StudentList.FirstOrDefault(x => x.Id == id);
+
+            if(student != null)
+            {
+                return View(student);
+            }
+
+            return NotFound();
+        }
+    }
+}
